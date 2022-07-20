62e1bb56c7b3ac74bc5d22784d2b079dc03eb4e4
Aleksey Matveev <matveevalex@yoomoney.ru>
2022-07-20 11:56:39 +0300

    Фикс названия файла

--- src/main/java/PassportGenerator.java
+++ src/main/java/PassportGenerator.java
@@ -6,10 +6,10 @@ import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class PassportGenerator {
-    private static final int amount = 100000;
+    private static final int amount = 10;
 
     public static void main(String[] args) throws FileNotFoundException {
-        File outputFile = new File("lists_of_expired_passports.csv");
+        File outputFile = new File("list_of_expired_passports.csv");
         try (PrintWriter pw = new PrintWriter(outputFile)) {
             pw.println("PASSP_SERIES,PASSP_NUMBER");
             List<String> values = new ArrayList<>();
